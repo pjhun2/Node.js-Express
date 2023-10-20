6b94397bb5be970ac2bab41a15a113e62ff8986c
ian <ian@bemyfriends.com>
2023-10-20 17:35:00 +0900

    인증구현 - 로그인 쿠키 생성

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,23 +1,23 @@
-var http= require('http');
-var cookie = require('cookie');
-
-http.createServer(function(request,response) {
-    console.log(request.headers.cookie)
-    var cookies = {};
-    if(request.headers.cookie !== undefined) {
-        cookies = cookie.parse(request.headers.cookie)
-    }
-    console.log(cookies.yummy)
-    response.writeHead(200, {
-        "Set-Cookie" : [
-            "yummy=choco",
-            "tasty=good",
-            `Permanent=cookie; Max-Age=${60*60*24*30}`,
-            'Secure=Secure; Secure',
-            'HttpOnly=HttpOnly; HttpOnly',
-            'Path=Path; Path=/cookie',
-            'Domain=Domain; Domain=o2.org'
-        ]
-    })
-    response.end('cookie')
-}).listen(3000);
+// var http= require('http');
+// var cookie = require('cookie');
+//
+// http.createServer(function(request,response) {
+//     console.log(request.headers.cookie)
+//     var cookies = {};
+//     if(request.headers.cookie !== undefined) {
+//         cookies = cookie.parse(request.headers.cookie)
+//     }
+//     console.log(cookies.yummy)
+//     response.writeHead(200, {
+//         "Set-Cookie" : [
+//             "yummy=choco",
+//             "tasty=good",
+//             `Permanent=cookie; Max-Age=${60*60*24*30}`,
+//             'Secure=Secure; Secure',
+//             'HttpOnly=HttpOnly; HttpOnly',
+//             'Path=Path; Path=/cookie',
+//             'Domain=Domain; Domain=o2.org'
+//         ]
+//     })
+//     response.end('cookie')
+// }).listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const template = require("../lib/template");
 const express = require('express')
+const fs = require("fs");
 const router = express.Router()
 router.get('/', function (req,res){
     var title = 'Welcome';
@@ -11,6 +12,40 @@ router.get('/', function (req,res){
         `
         , `<a href="/topic/create">create</a>`);
     res.send(html);
+})
+
+router.get('/login', function (req,res){
+    var title = 'Login';
+    var list = template.list(req.list);
+    var html = template.html(title, list, `
+        <form action="/login_process" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="submit"></p>
+        </form>
+        `
+        ,`<a href="/topic/create">create</a>`);
+    res.send(html);
+})
+
+router.post('/login_process', function (req,res){
+    var post = req.body
+    var email = post.email
+    var password = post.password
+    if ( email === "ian@bemyfriends.com" && password === "pw.1234") {
+        res.writeHead(302, {
+            'Set-Cookie' : [
+                `email=${email}`,
+                `password=${password}`,
+                `nickname=ian`
+            ],
+            location: '/'
+        })
+        res.end();
+    } else {
+        res.end('Who ?');
+    }
+    res.end();
 })
 
 module.exports = router
