de6b7bb8c961740583c9cff696111e1a95a50a20
ian <ian@bemyfriends.com>
2023-10-25 22:47:10 +0900

    삭제 수정 권한제어 구현

--- routes/topic.js
+++ routes/topic.js
@@ -30,7 +30,7 @@ var shortid = require('shortid')
 //     return authStatusUI
 // }
 router.get('/create', (req, res) => {
-    if(!auth.isOwner(req,res)) {
+    if (!auth.isOwner(req, res)) {
         res.redirect('/')
         return false
     }
@@ -48,12 +48,12 @@ router.get('/create', (req, res) => {
                     <input type="submit">
                 </p>
             </form>
-            `,'',auth.statusUI(req, res));
+            `, '', auth.statusUI(req, res));
     res.send(html);
 })
 
 router.post('/create_process', (req, res) => {
-    if(!auth.isOwner(req,res)) {
+    if (!auth.isOwner(req, res)) {
         res.redirect('/')
         return false
     }
@@ -62,10 +62,10 @@ router.post('/create_process', (req, res) => {
     var description = post.description
     var id = shortid.generate()
     db.get('topics').push({
-        id:id,
-        title:title,
-        description:description,
-        user_id:req.user.id
+        id: id,
+        title: title,
+        description: description,
+        user_id: req.user.id
     }).write();
     res.redirect(`/topic/${id}`)
     // fs.writeFile(`./data/${title}`,description,'utf8', function (err) {
@@ -75,18 +75,22 @@ router.post('/create_process', (req, res) => {
 })
 
 router.get('/update/:pageId', (req, res) => {
-    if(!auth.isOwner(req,res)) {
+    if (!auth.isOwner(req, res)) {
         res.redirect('/')
         return false
     }
-    var filteredId = path.parse(req.params.pageId).base
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = req.params.pageId;
-        var list = template.list(req.list);
-        var html = template.html(title, list, `
+    var topic = db.get('topics').find({id: req.params.pageId}).value();
+    if(topic.user_id !== req.user.id) {
+        req.flash('error', '수정 권한이 없습니다.');
+        return res.redirect('/')
+    }
+    var title = topic.title
+    var description = topic.description
+    var list = template.list(req.list);
+    var html = template.html(title, list, `
 
                 <form action="/topic/update_process" method="post">
-                  <input type="hidden" name="id" value="${title}">
+                  <input type="hidden" name="id" value="${topic.id}">
                   <p><input type="text" name="title" placeholder="title" value="${title}"></p>
                     <p>
                         <textarea name="description" placeholder="description">
@@ -98,52 +102,71 @@ router.get('/update/:pageId', (req, res) => {
                     </p>
                 </form>
                 `,
-            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,auth.statusUI(req,res));
-        res.send(html);
-    });
+        `<a href="/topic/create">create</a> <a href="/topic/update/${topic.id}">update</a>`, auth.statusUI(req, res));
+    res.send(html);
 })
 
 router.post('/update_process', (req, res) => {
-    if(!auth.isOwner(req,res)) {
+    if (!auth.isOwner(req, res)) {
         res.redirect('/')
         return false
     }
+
     var post = req.body
     var id = post.id
     var title = post.title
     var description = post.description
-    fs.rename(`./data/${id}`,`./data/${title}`,function (err){
-        fs.writeFile(`./data/${title}`,description,'utf8', function (err){
-            res.writeHead(302, {Location: `/topic/${title}`});
-            res.end();
-        })
-    })
+    var topic = db.get('topics').find({id:id}).value()
+    if(topic.user_id !== req.user.id) {
+        req.flash('error', '수정 권한이 없습니다.');
+        return res.redirect('/')
+    }
+
+    db.get('topics').find({id:id}).assign({
+        title:title,
+        description:description
+    }).write()
+    res.redirect(`/topic/${topic.id}`);
+    // fs.rename(`./data/${id}`, `./data/${title}`, function (err) {
+    //     fs.writeFile(`./data/${title}`, description, 'utf8', function (err) {
+    //         res.writeHead(302, {Location: `/topic/${title}`});
+    //         res.end();
+    //     })
+    // })
 })
 
 router.post('/delete_process', (req, res) => {
-    if(!auth.isOwner(req,res)) {
+    if (!auth.isOwner(req, res)) {
         res.redirect('/')
         return false
     }
     var post = req.body
     var id = post.id
-    var filteredId = path.parse(id).base
-    fs.unlink(`./data/${filteredId}`, function (error){
-        res.writeHead(302, {Location: `/`});
-        res.end();
-    })
+    var topic = db.get('topics').find({id:id}).value();
+    if(topic.user_id !== req.user.id) {
+        req.flash('error', '삭제 권한이 없습니다.');
+        return res.redirect('/')
+    }
+
+    db.get('topics').remove({id:id}).write()
+    res.redirect('/')
+
+    // fs.unlink(`./data/${filteredId}`, function (error) {
+    //     res.writeHead(302, {Location: `/`});
+    //     res.end();
+    // })
 })
 router.get('/:pageId', (req, res, next) => {
 
-    var topic = db.get('topics').find({id:req.params.pageId}).value()
+    var topic = db.get('topics').find({id: req.params.pageId}).value()
     var title = topic.title;
     var description = topic.description
-    var user = db.get('users').find({id:topic.user_id}).value()
-    var sanitizedTitle =  sanitizeHtml(title)
+    var user = db.get('users').find({id: topic.user_id}).value()
+    var sanitizedTitle = sanitizeHtml(title)
     var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: [ 'h1' ],
+        allowedTags: ['h1'],
         allowedAttributes: {
-            'a': [ 'href' ]
+            'a': ['href']
         },
         allowedIframeHostnames: ['www.youtube.com']
     });
@@ -154,12 +177,12 @@ router.get('/:pageId', (req, res, next) => {
         <p>by ${user.displayName}</p>
         `,
         `<a href="/topic/create">create</a>
-               <a href="/topic/update/${sanitizedTitle}">update</a>
+               <a href="/topic/update/${topic.id}">update</a>
                <form action="/topic/delete_process" method="post">
-               <input type="hidden" name="id" value="${sanitizedTitle}">
+               <input type="hidden" name="id" value="${topic.id}">
                <input type="submit" value="delete">
                </form>
-           `,auth.statusUI(req,res));
+           `, auth.statusUI(req, res));
     res.send(html);
 
 })
